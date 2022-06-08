a3af8b801b605f32f7175fd1150eae3abc5adff6
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2022-06-08 02:50:04 -0400

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { HomePageComponent } from './pages/home-page';
+import { ServicePageComponent } from './pages/service-page';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'services', component: ServicePageComponent},
+  {path: '', component: HomePageComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/pages/service-page/service-page.component.ts
+++ src/app/pages/service-page/service-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IDocument } from 'src/app/models/IDocument';
+import { IFlowerCard } from 'src/app/models/IFlowerCard';
+import { FlowerFilerServiceService } from 'src/app/services/flower-filer-service.service';
+
 
 @Component({
   selector: 'app-service-page',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-page.component.css']
 })
 export class ServicePageComponent implements OnInit {
-
-  constructor() { }
+  protected flowerCards!: IFlowerCard[][];
+  protected slides: IDocument[] = [
+    {
+      src: "assets/homeCarousel/slide-1.jpg",
+      title: "Pink Peonies",
+      description: "hi."
+    },
+    {
+      src: "assets/homeCarousel/slide-2.jpg",
+      title: "Colorful Flowers",
+      description: "hi2."
+    },
+    {
+      src: "assets/homeCarousel/slide-3.jpg",
+      title: "Colorful Flowers",
+      description: "hi2."
+    }
+  ];
+  constructor(private flowerFilerServiceService: FlowerFilerServiceService) { }
 
   ngOnInit(): void {
+    this.flowerCards = this.flowerFilerServiceService.getCardMatrix(3, 2);
   }
+
+  applyFilter(filter: string) {
+    this.flowerCards = this.flowerFilerServiceService.getFlowerCardsFiltered(3,filter);
+  }
+  
+  scroll(el: HTMLElement) {
+    el.scrollIntoView();
+ }
 
 }
